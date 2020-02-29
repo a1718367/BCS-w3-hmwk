649b19bcfa12cdb7bc2a3bc0eaea945c4598fc03
PzOng <pangszee.ong@gmail.com>
2020-02-29 21:09:54 +1030

    functional

--- javascript.js
+++ javascript.js
@@ -1,4 +1,7 @@
-    const pwl = document.getElementById("length");
+window.onload = function(){document.getElementById("length").focus()};   
+   
+   
+   const pwl = document.getElementById("length");
     const lowcase = "abcdefghijklmnopqrstuvwxyz";
     const upcase= "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     const numeric = "0123456789";
@@ -9,20 +12,28 @@
     const nsymbols = Array.from(symbols);
 
 
-function validate(){
-    if(length_el.value == "" || length_el.value <=0 || length_el.value > 128){
-        document.getElementById("error_nolength").innerHTML = "Please eneter a number between 8 and 128";
-    }else {
-        document.getElementById("error_nolength").innerHTML = "";
+function initiate(){
+    if(pwl.value == "" || pwl.value < 8 || pwl.value > 128){
+        document.getElementById("result").innerHTML = "Please enter number between 8 and 128";
+        document.getElementById("result").style.color = "red";
+        return;
+    }else{
+        document.getElementById("err_length").innerHTML = "";
+        generate();
     }
 }
 
+
 function generate(){
+
     const pwl = document.getElementById("length");
     const cblowcase = document.getElementById("lowcase");
     const cbupcase = document.getElementById("upcase");
     const cbnumeric = document.getElementById("numeric");
     const cbsymbols = document.getElementById("symbols");
+    var result = "";
+    let pwlength = +pwl.value;
+
     if (cblowcase.checked == true){
         arr1 = lowcase;
     } else {
@@ -47,13 +58,17 @@ function generate(){
     console.log(arr1,arr2,arr3,arr4)
     f = arr1 + arr2 + arr3 + arr4;
 
-var result = "";
-let pwlength = +pwl.value;
-for (var i = 0; i<pwlength; i++){
-result = result + f.charAt(Math.floor(Math.random()*Math.floor(f.length)))
+if(f.length == 0){
+        document.getElementById("result").innerHTML = "Select at least 1 criteria";
+        document.getElementById("result").style.color = "red";
+        return;
+    }else{
+    for (var i = 0; i<pwlength; i++){
+    result = result + f.charAt(Math.floor(Math.random()*Math.floor(f.length)))
 }
-console.log(f, result, result.length);
-document.getElementById("result").innerHTML = result;
-document.getElementById("display").innerHTML += result + "</br>";
-
+    console.log(f, result, result.length);
+    document.getElementById("result").style.color = "black";
+    document.getElementById("result").innerHTML = result;
+    document.getElementById("display").innerHTML += result + "</br>";
+    }
 }
